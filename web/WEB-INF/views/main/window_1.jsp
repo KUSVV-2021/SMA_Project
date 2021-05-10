a8c2c3c5519bf30f813664ef77fff630bdbd4232
unknown <com8599@gmail.com>
2021-05-10 12:33:58 +0900

    11번 유즈케이스 반영

--- web/WEB-INF/views/main/window_1.jsp
+++ web/WEB-INF/views/main/window_1.jsp
@@ -10,12 +10,15 @@
 <head>
     <title>${Title}</title>
     <link href="/resources/css/base.css" rel="stylesheet" type="text/css">
+    <link href="/resources/css/base1.css" rel="stylesheet" type="text/css">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 </head>
 <body>
     <div class="main_view">
         결제 카드를 삽입해주세요<br>
         <input onclick="location.href='../window3'" class="input_code" type="text" placeholder="선결제 코드 입력"/>
+        <br><br><br>
+        <button onclick="openKeyPad()">보안코드입력</button>
     </div>
     <script src="/resources/js/window_1.js"></script>
 </body>
--- web/resources/js/window_1.js
+++ web/resources/js/window_1.js
@@ -1,3 +1,90 @@
 window.onkeydown = function (e) {
     if (e.code == 'KeyC') location.href='../window2';
 }
+
+let input = "";
+
+function openKeyPad() {
+    input = "";
+    const div = document.createElement("div");
+    div.classList.add("key_pad");
+    const table = document.createElement("table");
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+    td.innerText = "1";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "2";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "3";
+    tr.appendChild(td);
+    table.appendChild(tr);
+    tr = document.createElement("tr");
+    td = document.createElement("td");
+    td.innerText = "4";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "5";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "6";
+    tr.appendChild(td);
+    table.appendChild(tr);
+    tr = document.createElement("tr");
+    td = document.createElement("td");
+    td.innerText = "7";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "8";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "9";
+    tr.appendChild(td);
+    table.appendChild(tr);
+    tr = document.createElement("tr");
+    td = document.createElement("td");
+    td.innerText = "0";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "delete";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerText = "enter";
+    tr.appendChild(td);
+    table.appendChild(tr);
+    div.appendChild(table);
+    const main = document.getElementsByClassName("main_view")[0];
+    main.onclick = (e) => removePad(e);
+    main.appendChild(div);
+    $('.key_pad td').click( (e) => clickPad(e) );
+}
+
+let chk = false;
+
+function removePad(e) {
+    if ( !chk ) chk = true;
+    else {
+        const div = document.getElementsByClassName("key_pad")[0];
+        const x = e.pageX;
+        const y = e.pageY;
+        if ( !( x >= div.offsetLeft && x <= ( div.offsetLeft + div.offsetWidth ) && y >= div.offsetTop && y <= ( div.offsetTop + div.offsetHeight ) ) ) {
+            div.remove();
+            chk = false;
+        }
+    }
+}
+
+function clickPad(e) {
+    if ( e.target.innerText == "delete" ) {
+        input = input.slice(0, -1)
+    } else if ( e.target.innerText == "enter" ) {
+        if (input == "1234") {
+            location.href="../window12";
+        } else {
+            alert("잘못된 입력입니다.");
+        }
+    } else {
+        input += e.target.innerText;
+    }
+}
