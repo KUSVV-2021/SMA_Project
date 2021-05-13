f21bfa35214d113de64c2e42c905d030e512a681
unknown <com8599@gmail.com>
2021-05-13 15:31:13 +0900

    testcase add

---
+++ src/test/Controller/TestController.java
@@ -0,0 +1,132 @@
+package Controller;
+
+import org.junit.Test;
+import org.springframework.stereotype.Controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class TestController {
+
+    private static final Object INDEX = 1;
+    private static final Object SEQ = 1;
+    private static final Object D_NAME = "칠성사이다";
+
+    @Test
+    public void getDrinkList() throws IOException {
+        URL obj = null;
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("INDEX", INDEX);
+
+        StringBuilder postData = new StringBuilder();
+        for(Map.Entry<String,Object> param : params.entrySet()) {
+            if(postData.length() != 0) postData.append('&');
+            postData.append(param.getKey());
+            postData.append('=');
+            postData.append(param.getValue());
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        obj = new URL("http://localhost:8080/MainProject/drink/getDrinkList");
+
+        HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        con.setDoOutput(true);
+        con.getOutputStream().write(postDataBytes);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "UTF-8"));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while((inputLine = in.readLine()) != null){
+            response.append(inputLine);
+        }
+        in.close();
+
+        String result = response.toString();
+    }
+
+    @Test
+    public void buyDrink() throws IOException {
+        URL obj = null;
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("INDEX", INDEX);
+        params.put("D_SEQ", SEQ);
+
+        StringBuilder postData = new StringBuilder();
+        for(Map.Entry<String,Object> param : params.entrySet()) {
+            if(postData.length() != 0) postData.append('&');
+            postData.append(param.getKey());
+            postData.append('=');
+            postData.append(param.getValue());
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        obj = new URL("http://localhost:8080/MainProject/drink/buyDrink");
+
+        HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        con.setDoOutput(true);
+        con.getOutputStream().write(postDataBytes);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while((inputLine = in.readLine()) != null){
+            response.append(inputLine);
+        }
+        in.close();
+
+        String result = response.toString();
+    }
+
+    @Test
+    public void getDrinkInfoFromOtherDVM() throws IOException {
+        URL obj = null;
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("INDEX", INDEX);
+        params.put("D_NAME", D_NAME);
+
+        StringBuilder postData = new StringBuilder();
+        for(Map.Entry<String,Object> param : params.entrySet()) {
+            if(postData.length() != 0) postData.append('&');
+            postData.append(param.getKey());
+            postData.append('=');
+            postData.append(param.getValue());
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        obj = new URL("http://localhost:8080/MainProject/drink/getDrinkInfoFromOtherDVM");
+
+        HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        con.setDoOutput(true);
+        con.getOutputStream().write(postDataBytes);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while((inputLine = in.readLine()) != null){
+            response.append(inputLine);
+        }
+        in.close();
+
+        String result = response.toString();
+    }
+
+}
