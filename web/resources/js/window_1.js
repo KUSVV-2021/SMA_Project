008dcae40fc1ddd2186b79399a8dbd4da6a35dec
unknown <com8599@gmail.com>
2021-05-13 11:58:17 +0900

    수정

--- web/resources/js/window_1.js
+++ web/resources/js/window_1.js
@@ -1,5 +1,13 @@
+let ls = localStorage.getItem("IDX_DVM");
+if (ls == null || ls == "" || ls == 0) {
+    localStorage.setItem("IDX_DVM", 1+"");
+    ls = 1;
+} else {
+    ls = ls*1;
+}
+
 window.onkeydown = function (e) {
-    if (e.code == 'KeyC') location.href='../window2';
+    if (e.code == 'KeyC') location.href='../window2?INDEX='+ls;
 }
 
 let input = "";
